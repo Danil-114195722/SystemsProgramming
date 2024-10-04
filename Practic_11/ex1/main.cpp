49fa192cbdb081b0fcf0cc7c4ce05abeccac7839
Danil-114195722 <ejyou.user@gmail.com>
2024-10-04 16:42:46 +0300

    Changed main in ex1 in Practic_11

--- Practic_11/ex1/main.cpp
+++ Practic_11/ex1/main.cpp
@@ -4,14 +4,34 @@
 
 
 int main() {
-	char word[] = "hello, world";
-	std::cout << "word: " << word << std::endl;
+	char word[256];
+	std::cout << "Hello!" << std::endl << "It's a Caesar cipher encoder/decoder." << std::endl << std::endl;
 
-	EncodeWord(word, 5);
-	std::cout << "encrypted word: " << word << std::endl;
+	char mode;
+	std::cout << "Enter mode [E (e) - for encode | D (d) - for decode | any - for exit] ";
+	std::cin >> mode;
+	int shift;
 
-	DecodeWord(word, 5);
-	std::cout << "decrypted word: " << word << std::endl;
+	while (mode == 'E' || mode == 'e' || mode == 'D' || mode == 'd') {
+		std::cout << "Enter the shift for encoding/decoding [natural number] ";
+		std::cin >> shift;
+
+		std::cout << "Enter the word below:" << std::endl;
+		std::cin >> word;
+
+		if (mode == 'E' || mode == 'e') {
+			EncodeWord(word, shift);
+			std::cout << "Encoded word:" << std::endl << word << std::endl << std::endl;
+		} else {
+			DecodeWord(word, shift);
+			std::cout << "Decoded word:" << std::endl << word << std::endl << std::endl;
+		}
+
+		std::cout << "Enter mode [E (e) - for encode | D (d) - for decode | any - for exit] ";
+		std::cin >> mode;
+	}
+
+	std::cout << "Goodbye!" << std::endl;
 
 	return 0;
 }
